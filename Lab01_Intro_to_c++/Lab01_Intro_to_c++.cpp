332ce63790ba361f6b5be7c9e8a1455b795a3ceb
Tauruie <96396660+Taurinic@users.noreply.github.com>
2025-01-29 15:29:35 +0000

    Degrees to Radians conversion
    Used 3 variables pi ,angleInDegrees and angleInRadians to calculate my result.
    
    angleInRadians = angleInDegrees * pi /180

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -4,7 +4,22 @@ int main()
 {
 	//Starting print code
 	std::cout << "Hello World! \n" << std::endl;
+
+
+	// Degrees to radians conversion
+	float angleInDegrees = 45.0f;
+	float pi = 3.1415927f;
+
+	float angleInRadians = angleInDegrees * pi / 180.0f;
+
+	// Output result
+	std::cout << angleInDegrees << " degrees is equal to "
+			  << angleInRadians << " radians." << std::endl;
+
 	return 0;
+
+
+
 
 
 

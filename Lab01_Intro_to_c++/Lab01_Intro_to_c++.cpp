671b262af93b33655ae82c48bacb318db3faeaff
Tauruie <96396660+Taurinic@users.noreply.github.com>
2025-01-29 15:39:06 +0000

    If statements
    Made a if else statement that determines if a angle is acute or obtuse. If over 90 degrees the angle is obtuse. Less than would be acute.

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -6,15 +6,28 @@ int main()
 	std::cout << "Hello World! \n" << std::endl;
 
 
-	// Degrees to radians conversion
-	float angleInDegrees = 45.0f;
+	// Degrees to radians conversion ----------------------------
+	float angleInDegrees = 180.0f;
 	float pi = 3.1415927f;
 
 	float angleInRadians = angleInDegrees * pi / 180.0f;
 
-	// Output result
+	// Output the result
 	std::cout << angleInDegrees << " degrees is equal to "
 			  << angleInRadians << " radians." << std::endl;
+	// ---------------------------------------------------------
+
+
+	//If statements---------------------------------------------
+	std::cout << "\nIf statement\n------------" << std::endl; // Prints text 
+	std::cout << angleInDegrees << " degrees is an"; //Pirnts angle then text 
+
+	if (angleInDegrees < 90) //if angle in degrees is less than 90
+		std::cout << " acute angle." << std::endl; //print actute aganle
+
+	else
+		std::cout << " Obtuse angle." << std::endl; //else print obtuse angle 
+
 
 	return 0;
 

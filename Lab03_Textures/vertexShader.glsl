81b68028d1a6086046c61621b9b61ca28a656336
Tauruie <96396660+Taurinic@users.noreply.github.com>
2025-02-12 15:42:24 +0000

    Shaders

--- Lab03_Textures/vertexShader.glsl
+++ Lab03_Textures/vertexShader.glsl
@@ -1,11 +1,20 @@
 #version 330 core
 
+//inputs 
 layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 uv;
 
-out vec3 Colour;
+
+//outputs 
+out vec2 UV;
+
 
 void main()
 {
     // Output vertex position
     gl_Position = vec4(position, 1.0);
+
+    //output texture coordinates 
+        
+        UV= uv;
 }

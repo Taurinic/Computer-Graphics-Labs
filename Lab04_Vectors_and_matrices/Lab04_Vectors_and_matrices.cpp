50ccd05ae1c1cbcdea70b4489e7af798c47b68c8
Tauruie <96396660+Taurinic@users.noreply.github.com>
2025-02-19 15:20:11 +0000

    Vectors
    Here we have declared two vectors a and b using the glm::vec3 type (for vectors in 2D we would use glm::vec2). The elements of the vectors a and b have been defined using two different methods: a has been defined using the glm::vec3(x, y, z) constructor and b by accessing the individual elements using array indexing.

--- Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
+++ Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
@@ -6,5 +6,26 @@
 
 int main() {
     
+    //Vectors 
+    printf("Vectors and matrices\n");
+    printf("------------------------------\n");
+
+
+    //Defining vectors 
+    glm::vec3 a, b;
+    a = glm::vec3(3.0f, 0.0f, 4.0f);
+    b[0] = 1.0f, b[1] = 2.0f, b[2] = 3.0f;
+
+    printf("Defining vectors\n");
+    std::cout << "a = " << a << std::endl; 
+    std::cout << "b = " << b << std::endl;
+
+
+
+
+
+
     return 0;
 }
+
+

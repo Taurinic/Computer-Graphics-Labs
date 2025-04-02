80ed2af67271cbbdc20f05e05f1c47b790311192
Tauruie <96396660+Taurinic@users.noreply.github.com>
2025-04-02 16:04:46 +0100

    10.3. Rotations using quaternions

--- common/maths.cpp
+++ common/maths.cpp
@@ -25,24 +25,11 @@ float Maths::radians(float angle)
 glm::mat4 Maths::rotate(const float& angle, glm::vec3 v)
 {
 	v = glm::normalize(v);
-	float c = cos(angle);
-	float s = sin(angle);
-	float x2 = v.x * v.x, y2 = v.y * v.y, z2 = v.z * v.z;
-	float xy = v.x * v.y, xz = v.x * v.z, yz = v.y * v.z;
-	float xs = v.x * s, ys = v.y * s, zs = v.z * s;
+	float c = cos(0.5f * angle);
+	float s = sin(0.5f * angle);
+	Quaternion q(c, s * v.x, s * v.y, s * v.z);
 
-	glm::mat4 rotate;
-	rotate[0][0] = (1 - c) * x2 + c;
-	rotate[0][1] = (1 - c) * xy + zs;
-	rotate[0][2] = (1 - c) * xz - ys;
-	rotate[1][0] = (1 - c) * xy - zs;
-	rotate[1][1] = (1 - c) * y2 + c;
-	rotate[1][2] = (1 - c) * yz + xs;
-	rotate[2][0] = (1 - c) * xz + ys;
-	rotate[2][1] = (1 - c) * yz - xs;
-	rotate[2][2] = (1 - c) * z2 + c;
-
-	return rotate;	
+	return q.matrix();
 }
 
 //Quaternions 
@@ -54,4 +41,26 @@ Quaternion::Quaternion(const float w, const float x, const float y, const float 
 	this->x = x;
 	this->y = y;
 	this->z = z;
+}
+
+glm::mat4 Quaternion::matrix()
+{
+	float s = 2.0f / (w * w + x * x + y * y + z * z);
+	float xs = x * s, ys = y * s, zs = z * s;
+	float xx = x * xs, xy = x * ys, xz = x * zs;
+	float yy = y * ys, yz = y * zs, zz = z * zs;
+	float xw = w * xs, yw = w * ys, zw = w * zs;
+
+	glm::mat4 rotate;
+	rotate[0][0] = 1.0f - (yy + zz);
+	rotate[0][1] = xy + zw;
+	rotate[0][2] = xz - yw;
+	rotate[1][0] = xy - zw;
+	rotate[1][1] = 1.0f - (xx + zz);
+	rotate[1][2] = yz + xw;
+	rotate[2][0] = xz + yw;
+	rotate[2][1] = yz - xw;
+	rotate[2][2] = 1.0f - (xx + yy);
+
+	return rotate;
 }
